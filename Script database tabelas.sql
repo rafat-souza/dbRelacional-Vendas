53f7ef0da78c2627da8b3a2eac980ae36b597e3f
Rafael Toledo Souza <ch1at@outlook.com>
2026-04-10 10:39:21 -0300

    Mais dados

--- Script database tabelas.sql
+++ Script database tabelas.sql
@@ -58,3 +58,4 @@ BEGIN
 END//
 
 DELIMITER ;
+
--- Testes de integridade.sql
+++ Testes de integridade.sql
@@ -6,10 +6,26 @@ INSERT INTO Cliente (nome, email, telefone) VALUES
 ('João Silva', 'joao@email.com', '11999999999'),
 ('Maria Oliveira', 'maria@email.com', '11888888888');
 
+INSERT INTO Cliente (nome, email, telefone) VALUES 
+('Pedro Souza', 'pedro@gmail.com', '11777777777'),
+('Sophia Araújo', 'sophia@hotmail.com', '11444444444'),
+('Luiza Cardoso', 'luiza@gmail.com', '12999999999'),
+('Luana Amoedo', 'luana@outlook.com', '11222222222'),
+('Ricardo Nunes', 'ricardo@gmail.com', '12888888888'),
+('Matheus Vieira', 'matheus@hotmail.com', '12777777777'),
+('Lucas Santos', 'lucas@gmail.com', '12555555555'),
+('Joana Pereira', 'joana@outlook.com', '11555555555');
+
 INSERT INTO Produto (nome, preco, id_categoria) VALUES 
 ('Notebook', 3500.00, 1),
 ('Smartphone', 2000.00, 1),
 ('Camiseta', 50.00, 2);
+
+INSERT INTO Produto (nome, preco, id_categoria) VALUES 
+('Vestido', 200.00, 2),
+('Calça', 80.00, 2),
+('Meias', 20.00, 2),
+('Moletom', 120.00, 2);
 
 INSERT INTO Pedido (id_cliente) VALUES 
 (1), 
@@ -46,3 +62,22 @@ DELETE FROM Categoria
 WHERE id_categoria = 3;
 
 SELECT * FROM Categoria;
+
+INSERT INTO Categoria (nome, descricao) VALUES 
+('Brinquedos', 'Brinquedos de todos os tipos');
+
+INSERT INTO Produto (nome, preco, id_categoria) VALUES 
+('Hotwheels', 50.00, 4),
+('Urso de pelúcia', 30.00, 4),
+('Boneca barbie', 70.00, 4),
+('Lego', 300.00, 4);
+
+SELECT * FROM Produto WHERE id_categoria = 4;
+
+SELECT p.id_produto, 
+p.nome AS nome_produto, 
+p.preco, 
+c.nome AS nome_categoria
+FROM Produto p
+INNER JOIN Categoria c ON p.id_categoria = c.id_categoria
+WHERE c.nome = 'brinquedos';
