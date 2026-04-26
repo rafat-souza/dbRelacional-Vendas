d32a96ec8f9b4057888a2eb1f0df217d9715c464
Rafael Toledo Souza <ch1at@outlook.com>
2026-04-26 14:08:52 -0300

    View para usuários de only read

--- objetos_seguranca.sql
+++ objetos_seguranca.sql
@@ -4,3 +4,10 @@ ALTER TABLE Cliente ADD COLUMN senha_hash VARCHAR(256);
 UPDATE Cliente 
 SET senha_hash = SHA2('senha_padrao_123', 256) 
 WHERE id_cliente =1;
+
+-- View limitada da tabela de clientes
+CREATE VIEW vw_cliente_seguro AS
+SELECT id_cliente, nome, email
+FROM Cliente;
+
+SELECT * FROM vw_cliente_seguro;
