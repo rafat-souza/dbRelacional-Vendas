94447a41a7b21307ec8354846edfa9a00263118e
Rafael Toledo Souza <ch1at@outlook.com>
2026-04-26 15:04:30 -0300

    Teste inserção cliente user_aplicacao

--- usuarios_roles.sql
+++ usuarios_roles.sql
@@ -20,7 +20,7 @@ GRANT SELECT ON SistemaVendas.Item_Pedido TO 'role_leitura';
 GRANT SELECT ON SistemaVendas.Log_Auditoria_Preco TO 'role_leitura';
 GRANT SELECT ON SistemaVendas.vw_cliente_seguro TO 'role_leitura';
 
--- Testes - Criação de usuários
+-- Criação de usuários
 CREATE USER 'dba_admin'@'localhost' IDENTIFIED BY 'Admin!Forte2026';
 CREATE USER 'user_aplicacao'@'localhost' IDENTIFIED BY 'App@Sistema2026';
 CREATE USER 'user_relatorio'@'localhost' IDENTIFIED BY 'Read#Apenas2026';
@@ -33,10 +33,15 @@ SET DEFAULT ROLE ALL TO 'dba_admin'@'localhost', 'user_aplicacao'@'localhost', '
 
 FLUSH PRIVILEGES;
 
--- Testes - Acesso negado
--- Logado como user_relatorio'@'localhost
+-- Testes
+-- Logado como user_relatorio
 SELECT * FROM Cliente;
 SELECT * FROM vw_cliente_seguro;
 
--- Logado como user_aplicacao'@'localhost
+-- Logado como user_aplicacao
 DELETE FROM Categoria WHERE id_categoria = 1;
+INSERT INTO Cliente (nome, email, telefone) VALUES 
+('Mariazinha', 'mariaa@gmail.com', '11950994979');
+
+-- Logado como dba_admin
+DELETE FROM Produto WHERE id_produto = 15;
