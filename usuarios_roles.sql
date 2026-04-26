3f2c2555986b05add425696802b7bbf0e93dba96
Rafael Toledo Souza <ch1at@outlook.com>
2026-04-26 14:41:51 -0300

    Usuários e roles

---
+++ usuarios_roles.sql
@@ -0,0 +1,41 @@
+-- Criação das roles
+CREATE ROLE 'role_admin', 'role_app', 'role_leitura';
+
+-- Permissão administrador
+GRANT ALL PRIVILEGES ON SistemaVendas.* TO 'role_admin';
+
+-- Permissões aplicação (acesso operacional, mas não administrativo)
+GRANT SELECT, INSERT, UPDATE ON SistemaVendas.Cliente TO 'role_app';
+GRANT SELECT, INSERT, UPDATE ON SistemaVendas.Pedido TO 'role_app';
+GRANT SELECT, INSERT ON SistemaVendas.Item_Pedido TO 'role_app';
+GRANT SELECT ON SistemaVendas.Produto TO 'role_app';
+GRANT SELECT ON SistemaVendas.Categoria TO 'role_app';
+GRANT EXECUTE ON PROCEDURE SistemaVendas.sp_realizar_compra TO 'role_app'; 
+
+-- Permissões somente leitura (Relatórios/Analistas)
+GRANT SELECT ON SistemaVendas.Pedido TO 'role_leitura';
+GRANT SELECT ON SistemaVendas.Categoria TO 'role_leitura';
+GRANT SELECT ON SistemaVendas.Produto TO 'role_leitura';
+GRANT SELECT ON SistemaVendas.Item_Pedido TO 'role_leitura';
+GRANT SELECT ON SistemaVendas.Log_Auditoria_Preco TO 'role_leitura';
+GRANT SELECT ON SistemaVendas.vw_cliente_seguro TO 'role_leitura';
+
+-- Testes - Criação de usuários
+CREATE USER 'dba_admin'@'localhost' IDENTIFIED BY 'Admin!Forte2026';
+CREATE USER 'user_aplicacao'@'localhost' IDENTIFIED BY 'App@Sistema2026';
+CREATE USER 'user_relatorio'@'localhost' IDENTIFIED BY 'Read#Apenas2026';
+
+GRANT 'role_admin' TO 'dba_admin'@'localhost';
+GRANT 'role_app' TO 'user_aplicacao'@'localhost';
+GRANT 'role_leitura' TO 'user_relatorio'@'localhost';
+
+SET DEFAULT ROLE ALL TO 'dba_admin'@'localhost', 'user_aplicacao'@'localhost', 'user_relatorio'@'localhost';
+
+-- Testes - Acesso negado
+-- Logado como user_relatorio'@'localhost
+SELECT * FROM Cliente;
+SELECT * FROM vw_cliente_seguro;
+
+-- Logado como user_aplicacao'@'localhost
+DELETE FROM Categoria WHERE id_categoria = 1;
+FLUSH PRIVILEGES;
